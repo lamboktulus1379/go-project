fbbbcefc95643e6506db39723b4855d01f7cc5e5
Lambok Tulus <lamboktulus1379@gmail.com>
2025-05-10 11:34:20 +0700

    Fix test.

--- infrastructure/utils/util_test.go
+++ infrastructure/utils/util_test.go
@@ -13,12 +13,12 @@ func TestGetCurrentTime(t *testing.T) {
 	}{
 		{
 			name: "TestGetCurrentTime - 1",
-			want: time.Now().UTC(),
+			want: time.Now().UTC().Truncate(time.Second),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetCurrentTime(); !reflect.DeepEqual(got, tt.want) {
+			if got := GetCurrentTime().Truncate(time.Second); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetCurrentTime() = %v, want %v", got, tt.want)
 			}
 		})
