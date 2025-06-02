61fde0c9490af31be8e8073a7fd8914404105139
Lambok Tulus <lamboktulus1379@gmail.com>
2025-06-02 22:57:08 +0700

    Add test files for various components and update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,3 @@
-$env:ENV='stage'; go test
+#!/bin/bash
+export ENV='stage'
+go test ./... -cover -coverprofile=coverage.out && go tool cover -html=coverage.out -o coverage.html && go tool cover -func=coverage.out -o cover.txt
